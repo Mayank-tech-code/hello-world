b723245679ce211b561e7495871b038f0c21bf75
Mayank-tech-code <70426621+Mayank-tech-code@users.noreply.github.com>
2021-02-20 15:56:14 +0530

    initial commit

---
+++ practise.cpp
@@ -0,0 +1,20 @@
+# include<iostream>
+using namespace std
+int main()
+{
+	int arr[5];
+	cout<<"Enter a int inn ann arr";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Unsorted aray";
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i];
+		
+	}
+	cout<<endl;
+	
+	return 0;
+}
